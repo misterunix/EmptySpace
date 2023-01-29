d6e25da674265a618220f3cda22d96001f4e7635
Bill Jones <misterunix@gmail.com>
2023-01-28 22:35:52 -0600

    #1 Changed some variable names to be easier to read. I know this goes agist BP but I like it better.

--- dbroutines.go
+++ dbroutines.go
@@ -12,18 +12,22 @@ import (
 // Retuns the sql statement as a string.
 // This is a work in progress.
 func CreateTableFromStruct(table string, s interface{}) string {
-	var e reflect.Value = reflect.ValueOf(s)
+
+	var reflectedValue reflect.Value = reflect.ValueOf(s) // reflect the struct (interface)
+
 	var sqlstatement string
+
 	sqlstatement1 := "CREATE TABLE IF NOT EXISTS " + table + " ("
-	for i := 0; i < e.NumField(); i++ {
+	for i := 0; i < reflectedValue.NumField(); i++ {
 		var vt string
-		varName := e.Type().Field(i).Name
+		varName := reflectedValue.Type().Field(i).Name // get the name of the field
 		sqlstatement += "," + varName + " "
-		//sqlstatement += varName + " "
-		varType := e.Type().Field(i).Type
+		varType := reflectedValue.Type().Field(i).Type // get the type of the field
+
+		// Did this differnt than the other reflect code. This is a work in progress.
 		switch varType.Kind() {
 		case reflect.Int:
-			if varName == "ID" {
+			if varName == "ID" { // detect if the field is the ID field
 				vt = "INTEGER NOT NULL PRIMARY KEY"
 			} else {
 				vt = "INTEGER"
@@ -37,45 +41,33 @@ func CreateTableFromStruct(table string, s interface{}) string {
 		}
 		sqlstatement += vt
 	}
-	// such a crappy way to do this
-	sqlstatement = sqlstatement[1:]
+
+	// such a crappy way to do this. Return to this at a later date.
+	sqlstatement = sqlstatement[1:] // remove the first comma
 	sqlstatement += ")"
 	sqlstatement = sqlstatement1 + sqlstatement
 
 	return sqlstatement
 }
 
-/*
-func InsertStructSlice(table string, s []interface{}) string {
-	var sqls string
-	for _, v := range s {
-		sqls += InsertIntoTable(table, v)
-	}
-	return sqls
-}
-*/
-
 func InsertIntoTable(table string, s interface{}) string {
-
-	//	fmt.Println("InsertIntoTable:", table)
 
 	var middlesql1 string
 	var middlesql2 string
 
-	var e reflect.Value = reflect.ValueOf(s)
+	var reflectedValue reflect.Value = reflect.ValueOf(s)
 
 	middlesql1 = "INSERT INTO " + table + " ("
 	middlesql2 = ")VALUES("
-	for i := 0; i < e.NumField(); i++ {
-		//var vt string
-		varName := e.Type().Field(i).Name
-		//sqlstatement += "," + varName + " "
-		//sqlstatement += varName + " "
-		varType := e.Type().Field(i).Type
+	for i := 0; i < reflectedValue.NumField(); i++ {
 
-		varValue := e.Field(i).Interface()
+		varName := reflectedValue.Type().Field(i).Name
+		varType := reflectedValue.Type().Field(i).Type
+		varValue := reflectedValue.Field(i).Interface()
 
 		middlesql1 += varName + ","
+
+		// This is my normal way of working with reflect. Strings may be slower but easier to read.
 		switch varType.Name() {
 		case "int":
 			middlesql2 += fmt.Sprintf("%d", varValue.(int)) + ","
@@ -88,61 +80,8 @@ func InsertIntoTable(table string, s interface{}) string {
 		default:
 			return ""
 		}
-
 	}
 
-	/*
-	   o := "BEGIN;\n"
-	   beginstatement, err := database.Prepare(o)
-
-	   	if err != nil {
-	   		log.Panicln(err)
-	   	}
-
-	   _, err = beginstatement.Exec()
-
-	   	if err != nil {
-	   		log.Panicln(err)
-	   	}
-
-	   	for _, v := range Systems {
-	   		//fmt.Println("Inserting system", ind)
-	   		var ssql, vsql string
-	   		e := reflect.ValueOf(&v).Elem()
-	   		for i := 0; i < e.NumField(); i++ {
-	   			varName := e.Type().Field(i).Name
-	   			ssql += "," + varName
-	   			varValue := e.Field(i).Interface()
-	   			vv := fmt.Sprintf("%v", varValue)
-	   			varType := e.Type().Field(i).Type
-	   			switch varType.Name() {
-	   			case "int":
-	   				vsql += "," + vv
-	   			case "string":
-	   				vsql += "," + "'" + vv + "'"
-	   			}
-	   		}
-	   		ssql = ssql[1:]
-	   		vsql = vsql[1:]
-	   		sqlstatement := "INSERT INTO system( " + ssql + " ) VALUES(" + vsql + ")"
-	   		//fmt.Println(sqlstatement)
-	   		statement, _ := database.Prepare(sqlstatement)
-	   		statement.Exec()
-	   	}
-
-	   o = "COMMIT;\n"
-	   commitstatement, err := database.Prepare(o)
-
-	   	if err != nil {
-	   		log.Panicln(err)
-	   	}
-
-	   _, err = commitstatement.Exec()
-
-	   	if err != nil {
-	   		log.Panicln(err)
-	   	}
-	*/
 	middlesql1 = middlesql1[:len(middlesql1)-1]
 	middlesql2 = middlesql2[:len(middlesql2)-1] + ");"
 	yyy := middlesql1 + middlesql2
